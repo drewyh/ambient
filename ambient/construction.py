d1561dee9bed0bd7bf7f0e75af3bffe230e4518b
Andrew Hale <20280470+drewyh@users.noreply.github.com>
2020-04-03 18:03:31 +0200

    Implement constructions with heat transfer (#4)
    Implement a layered construction model using the improved frequency domain regression model from X. Xu et al. "An improvement to frequency-domain regression method for calculating conduction transfer functions of building walls" and S. Wang and Y. Chen "Transient heat flow calculation for multilayer constructions using a frequency-domain regression method"

---
+++ ambient/construction.py
@@ -0,0 +1,387 @@
+"""Constructions for simulation."""
+
+# All references are to the paper:
+# Transient heat flow calculation for multilayer constructions
+# using a frequency-domain regression method
+# Wang and Chen
+# Building and Environment 38 (2003)
+
+from collections import defaultdict
+from dataclasses import dataclass
+from functools import reduce
+from typing import List, Tuple
+
+import numpy as np
+
+from numpy.polynomial.polynomial import polyzero, polyone
+
+_EPS = 1.0e-9
+
+
+def _calculate_frequencies(
+    lower_limit_power: int = -8, upper_limit_power: int = -3, num_freqs: int = 50
+) -> np.ndarray:
+    """Calculate the frequencies for the FDR method.
+
+    References:
+        Section 4, steps 1 and 2.
+    """
+    return np.logspace(lower_limit_power, upper_limit_power, num=num_freqs)
+
+
+def _calculate_response_functions(response_matrix: np.ndarray) -> np.ndarray:
+    """Calculate the response functions for FDR method.
+
+    References:
+        Section 2, equations 10, 11, and 12
+    """
+    response_functions = 1.0 / response_matrix[:, 0, 1]
+    response_functions = np.tile(response_functions, (3, 1))
+    response_functions[0] *= response_matrix[:, 0, 0]
+    response_functions[2] *= response_matrix[:, 1, 1]
+
+    return response_functions
+
+
+def _approximate_response_function(
+    response_function: np.ndarray,
+    frequencies: np.ndarray,
+    numerator_order: int,
+    denominator_order: int,
+    denominator_values: np.ndarray = None,
+) -> Tuple[np.ndarray, np.ndarray]:
+    """Approximate the transfer functions by rational functions."""
+    # calculate two parts of matrix from eq. 21
+    max_power = max(numerator_order, denominator_order)
+    powers = np.power(1.0j * frequencies[:, np.newaxis], np.arange(max_power + 1))
+
+    h_matrix = np.copy(powers[:, : numerator_order + 1])
+
+    g_vector = response_function
+
+    # account for case of solving for denominator too
+    if denominator_values is None:
+        d_comp = -1.0 * np.copy(powers[:, 1 : denominator_order + 1])
+        h_matrix = np.concatenate((h_matrix, d_comp * g_vector[:, np.newaxis]), axis=1)
+    else:
+        g_vector = g_vector * denominator_values
+
+    gamma = np.real(np.conj(h_matrix.T) @ h_matrix)
+    theta = 0.5 * np.real(
+        np.conj(h_matrix.T) @ g_vector + h_matrix.T @ np.conj(g_vector)
+    )
+
+    coeffs = np.linalg.solve(gamma, theta)
+    coeffs = np.insert(coeffs, numerator_order + 1, 1)
+
+    return (
+        np.flip(coeffs[: numerator_order + 1]),
+        np.flip(coeffs[numerator_order + 1 :]),
+    )
+
+
+def _calculate_residuals(
+    numerator_poly: np.poly, denominator_poly: np.poly, denominator_roots: np.ndarray
+) -> np.ndarray:
+    """See eq. 36."""
+    delta = np.zeros_like(denominator_roots)
+    for idx, root in enumerate(denominator_roots):
+        root_poly = np.poly1d((1.0, -root))
+        alpha_prime_coeff, rem = np.polydiv(denominator_poly, root_poly)
+
+        assert np.allclose(rem, np.poly1d(polyzero), rtol=0.0, atol=1.0e-7)
+
+        alpha_prime = np.poly1d(alpha_prime_coeff)
+
+        delta_n = -1.0 * numerator_poly(root) / (root ** 2 * alpha_prime(root))
+        delta[idx] = delta_n
+
+    return delta
+
+
+@dataclass
+class Construction:
+    """Class for layered constructions.
+
+    The order of layers is outside to inside.
+    """
+
+    # order of layers is outside to inside
+    thicknesses: np.array
+    conductivities: np.array
+    resistances: np.array
+    densities: np.array = None
+    specific_heats: np.array = None
+    timestep: int = 3600
+
+    def __post_init__(self) -> None:
+        """Set construction data not stored in fields."""
+        complex_layer_resistances = self.thicknesses / self.conductivities
+        self._layer_resistances = np.copy(self.resistances)
+        self._layer_resistances[np.isnan(self.resistances)] = complex_layer_resistances
+
+        self._thermal_transmittance = 1.0 / np.sum(self.layer_resistances)
+
+        self._ctfs = None
+
+        # allow fixing the minimum and maximum order of ctfs for testing
+        self._min_ctf_order = 2
+        self._max_ctf_order = 6
+
+    @property
+    def layer_count(self) -> int:
+        """Return the number of layers in the construction."""
+        return len(self.resistances)
+
+    @property
+    def layer_resistances(self) -> np.ndarray:
+        """Return the resistances of layers in the construction."""
+        return self._layer_resistances
+
+    @property
+    def thermal_transmittance(self) -> float:
+        """Return the thermal transimittance of the construction."""
+        return self._thermal_transmittance
+
+    @property
+    def ctfs(self) -> np.ndarray:
+        """Return the conduction transfer functions."""
+        if self._ctfs is None:
+            # paper notes typical orders of 3-5, however 6 may be required for heavy
+            # constructions. Additionally, 1 allows resistance only (or close to)
+            # constructions to be modelled.
+            coeffs = self._approximate_response_functions()
+            den = np.poly1d(coeffs[-1])
+            arr: List[np.ndarray] = []
+            for numerator_coeff in coeffs[:-1]:
+                num = np.poly1d(numerator_coeff)
+                arr.extend(self._calculate_ctf(num, den, self.timestep))
+
+            self._ctfs = np.array(arr[::2] + [arr[-1]])
+
+        return self._ctfs
+
+    def _calculate_response_matrix(self, frequencies: np.ndarray) -> np.ndarray:
+        invsqrt_layer_diffusivity = np.sqrt(
+            self.densities * self.specific_heats / self.conductivities
+        )
+
+        layer_resistance = self.thicknesses / self.conductivities
+
+        # common args for sinh/cosh
+        value_grid = np.outer(
+            self.thicknesses * invsqrt_layer_diffusivity, np.sqrt(frequencies * 1.0j)
+        )
+
+        # precompute sinh/cosh value
+        sinh = np.sinh(value_grid)
+        cosh = np.cosh(value_grid)
+
+        # set resistance only layers
+        b_component = np.repeat(
+            self.resistances[:, np.newaxis], len(frequencies), axis=1
+        ).astype(np.complex)
+
+        a_component = np.ones_like(b_component, dtype=np.complex)
+        d_component = np.ones_like(b_component, dtype=np.complex)
+        c_component = np.zeros_like(b_component, dtype=np.complex)
+
+        complex_layers = np.isnan(self.resistances)
+
+        # matrix components
+        a_component[complex_layers, :] = cosh
+        d_component[complex_layers, :] = cosh
+        b_component[complex_layers, :] = (
+            layer_resistance[:, np.newaxis] * sinh / value_grid
+        )
+        c_component[complex_layers, :] = (
+            value_grid * sinh / layer_resistance[:, np.newaxis]
+        )
+
+        layer_matrices = np.dstack((a_component, b_component, c_component, d_component))
+        layer_matrices = layer_matrices.reshape(layer_matrices.shape[:-1] + (2, 2))
+
+        # calculate the product over all layers
+        ms_matrix = reduce(np.matmul, layer_matrices)
+
+        return ms_matrix
+
+    def _approximate_response_functions(self) -> List[np.ndarray]:
+        """Approximate the transfer functions by rational functions."""
+        frequencies = _calculate_frequencies()
+        response_matrix = self._calculate_response_matrix(frequencies)
+        response_functions = _calculate_response_functions(response_matrix)
+
+        # calculate the best fit for approximation
+        coeff_map = defaultdict(list)
+        value_map = defaultdict(float)
+        for numerator_order in range(self._min_ctf_order, self._max_ctf_order + 1):
+            for denominator_order in range(numerator_order, self._max_ctf_order + 1):
+                key = (numerator_order, denominator_order)
+
+                coeffs = [None] * 4
+
+                # first calculate the cross term (Y) to fix the denominator
+                try:
+                    coeffs[1], coeffs[3] = _approximate_response_function(
+                        response_functions[1],
+                        frequencies,
+                        denominator_order=denominator_order,
+                        numerator_order=numerator_order,
+                    )
+                except np.linalg.LinAlgError:
+                    # catch the case of a singular matrix solution
+                    value_map[key] = np.inf
+                    continue
+
+                # calculate the value of the denominator at frequences
+                denom_values = np.polyval(coeffs[3], frequencies)
+
+                # calculate the diff with the exact
+                diff = (
+                    response_functions[1]
+                    - np.polyval(coeffs[1], frequencies) / denom_values
+                )
+                value_map[key] += np.real(np.sum(diff * diff.conjugate()))
+
+                # now calculate the other functions
+                for idx, response_function in enumerate(response_functions):
+                    if idx == 1:
+                        continue
+                    coeffs[idx], _ = _approximate_response_function(
+                        response_function,
+                        frequencies,
+                        denominator_order=denominator_order,
+                        numerator_order=numerator_order,
+                        denominator_values=denom_values,
+                    )
+
+                    # add to the diff
+                    diff = (
+                        response_function
+                        - np.polyval(coeffs[idx], frequencies) / denom_values
+                    )
+                    value_map[key] += np.real(np.sum(diff * diff.conjugate()))
+
+                coeff_map[key] = coeffs
+
+        return coeff_map[min(value_map, key=value_map.get)]
+
+    def _calculate_ctf(
+        self, numerator_poly: np.poly, denominator_poly: np.poly, timestep: int
+    ) -> Tuple[np.ndarray, np.ndarray]:
+        # step 4
+        roots = denominator_poly.roots
+        delta = _calculate_residuals(numerator_poly, denominator_poly, roots)
+
+        # XXX: delta is complex for some constructions, not sure why
+        sum_delta = np.sum(delta)
+        assert np.imag(sum_delta) == 0.0
+        sum_delta = np.real(sum_delta)
+
+        coeffs = np.ones((roots.shape[0], 2), dtype=np.complex)
+
+        # note that there is no -1.0 factor in the exponent since the paper
+        # assumes the roots are -si (see eq. 35)
+        coeffs[:, 0] = -1.0 * np.exp(roots * timestep)
+
+        # XXX: this solves the issue of nans for very low resistance constructions
+        coeffs[np.isinf(coeffs)] = 0.0
+
+        fac = np.array([-1.0, 1.0])
+
+        if coeffs.size > 0:
+            # note: convolution is analogous to polynomial multiplication
+            polytot = reduce(np.convolve, coeffs)
+        else:
+            polytot = polyone
+
+        numerator_poly = polytot * sum_delta
+
+        partial_numerator_sum = np.zeros_like(numerator_poly[1:], dtype=np.complex)
+        for idx, delta_i in enumerate(delta):
+            mask = np.ones_like(roots, dtype=bool)
+            mask[idx] = False
+            partial_numerator_sum += delta_i * reduce(np.convolve, coeffs[mask])
+
+        # XXX: delta is complex for some constructions, not sure why
+        assert np.all(
+            np.abs(np.imag(partial_numerator_sum[np.isfinite(partial_numerator_sum)]))
+            <= _EPS
+        )
+        partial_numerator_sum = np.real(partial_numerator_sum)
+
+        if partial_numerator_sum.size > 0:
+            numerator_poly -= np.convolve(partial_numerator_sum, fac)
+
+        numerator_poly = np.convolve(numerator_poly, fac)
+
+        assert np.abs(numerator_poly[-1]) <= _EPS
+
+        # this is a division by z
+        numerator_poly = numerator_poly[:-1]
+
+        numerator_poly += polytot * self.thermal_transmittance * timestep
+        numerator_poly /= timestep
+
+        assert np.all(
+            np.abs(np.imag(numerator_poly[np.isfinite(numerator_poly)])) <= _EPS
+        )
+        numerator_poly = np.real(numerator_poly)
+
+        # numerator_poly[np.abs(numerator_poly) <= _EPS] = 0.0
+
+        assert np.all(np.abs(np.imag(polytot[np.isfinite(polytot)])) <= _EPS)
+        polytot = np.real(polytot)
+
+        return numerator_poly, polytot
+
+    @property
+    def _timeseries_length(self) -> int:
+        return self.ctfs.shape[-1]  # pylint: disable=unsubscriptable-object
+
+    def calculate_heat_flux_inside(
+        self,
+        outside_temps: np.ndarray,
+        inside_temps: np.ndarray,
+        inside_heat_fluxes: np.ndarray,
+        current_index: int,
+    ) -> float:
+        """Calculated the inside heat fluxes."""
+        # determine the array values
+        take = range(current_index - self._timeseries_length + 1, current_index + 1)
+        outside_temps = outside_temps[take]
+        inside_temps = inside_temps[take]
+        inside_heat_fluxes = inside_heat_fluxes[take[:-1]]
+
+        # from eq. 34
+        qi_new = (
+            np.sum(self.ctfs[1] * outside_temps)
+            - np.sum(self.ctfs[3][:-1] * inside_heat_fluxes)
+            - np.sum(self.ctfs[2] * inside_temps)
+        )
+
+        return qi_new
+
+    def calculate_heat_flux_outside(
+        self,
+        outside_temps: np.ndarray,
+        inside_temps: np.ndarray,
+        outside_heat_fluxes: np.ndarray,
+        current_index: int,
+    ) -> float:
+        """Calculated the inside heat fluxes."""
+        # determine the array values
+        take = range(current_index - self._timeseries_length + 1, current_index + 1)
+        outside_temps = outside_temps[take]
+        inside_temps = inside_temps[take]
+        outside_heat_fluxes = outside_heat_fluxes[take[:-1]]
+
+        # from eq. 34
+        qo_new = (
+            -np.sum(self.ctfs[0] * outside_temps)
+            - np.sum(self.ctfs[3][:-1] * outside_heat_fluxes)
+            + np.sum(self.ctfs[2] * inside_temps)
+        )
+
+        return qo_new
---
+++ tests/test_constructions.py
@@ -0,0 +1,287 @@
+import numpy as np
+import pytest
+
+from ambient.construction import Construction
+
+CTF_EPS = 0.02
+HEAT_TRANSFER_EPS = 0.007
+HEAT_TRANSFER_SUM_EPS = 1.0e-8
+
+# fmt: off
+SOL_AIR = 273.15 + np.array(
+    (
+        24.4, 24.4, 23.8, 23.3, 23.3, 23.8,
+        25.5, 27.2, 29.4, 31.6, 33.8, 36.1,
+        43.3, 49.4, 53.8, 55.0, 52.7, 45.5,
+        30.5, 29.4, 28.3, 27.2, 26.1, 25.0,
+    )
+)
+# fmt: on
+
+
+def _calculate_sums(ctfs):
+    """Calculate the ratio of sums based on eq. 41."""
+    ctf_sum = np.sum(ctfs, axis=1)
+    ctf_sum[:-1] /= ctf_sum[-1]
+
+    return ctf_sum[:-1]
+
+
+def iterate_heat_transfer(con, temp_in, temp_out, qi, qo=None, iterations=100):
+    """Iterate the heat transfer calculation."""
+    for iteration in range(iterations):
+        for hour in range(24):
+            qi[hour] = con.calculate_heat_flux_inside(temp_out, temp_in, qi, hour)
+            if qo is not None:
+                qo[hour] = con.calculate_heat_flux_outside(temp_out, temp_in, qo, hour)
+
+
+def test_heat_transfer_wang_wall_iii():
+    """Calculate properties of wall III from:
+
+        Shengwei Wang, Youming Chen,
+        Transient heat flow calculation for multilayer constructions using
+            a frequency-domain regression method,
+        Building and Environment,
+        Volume 38, Issue 1,
+        2003,
+        Pages 45-61,
+        ISSN 0360-1323,
+        https://doi.org/10.1016/S0360-1323(02)00024-0.
+    """
+    con = Construction(
+        thicknesses=np.array([25.39, 101.59, 25.30, 19.05]) / 1000,
+        conductivities=np.array([0.6924, 1.7310, 0.0433, 0.7270]),
+        densities=np.array([1858, 2243, 32, 1602]),
+        specific_heats=np.array(
+            [837.4] * 4
+        ),  # note the values in the paper are incorrect
+        resistances=np.array([0.0586, np.nan, np.nan, np.nan, np.nan, 0.1206]),
+        timestep=3600,
+    )
+
+    # fmt: off
+    qe_paper = np.array(
+        (
+            11.646,  9.999,  8.517,  7.203,  6.016,  4.960,
+             4.081,  3.473,  3.209,  3.304,  3.755,  4.523,
+             5.584,  7.162,  9.498, 12.441, 15.591, 18.406,
+            20.260, 20.392, 19.019, 17.157, 15.247, 13.403,
+        )
+    )
+    # fmt: on
+
+    # initialise values
+    qe = np.zeros(24)
+    qo = np.zeros(24)
+    temp_in = np.full_like(SOL_AIR, 273.15 + 24)
+    temp_out = SOL_AIR
+
+    # the paper values are after the 4th iteration
+    iterate_heat_transfer(con, temp_in, temp_out, qe, qo, iterations=4)
+
+    # check the heat transfer into the room
+    assert np.allclose(qe, qe_paper, rtol=0.0, atol=0.01)
+    assert np.allclose(qe, qe_paper, rtol=0.002, atol=0.0)
+
+    # now do further iterations to check heat transfer convergence
+    iterate_heat_transfer(con, temp_in, temp_out, qe, qo, iterations=96)
+
+    # check inside and outside heat transfer
+    assert np.allclose(
+        np.sum(qe) + np.sum(qo), 0.0, rtol=0.0, atol=HEAT_TRANSFER_SUM_EPS
+    )
+
+
+def test_ctfs_wang_wall_iii():
+    """Calculate properties of wall III from:
+
+        Shengwei Wang, Youming Chen,
+        Transient heat flow calculation for multilayer constructions using
+            a frequency-domain regression method,
+        Building and Environment,
+        Volume 38, Issue 1,
+        2003,
+        Pages 45-61,
+        ISSN 0360-1323,
+        https://doi.org/10.1016/S0360-1323(02)00024-0.
+    """
+    con = Construction(
+        thicknesses=np.array([25.39, 101.59, 25.30, 19.05]) / 1000,
+        conductivities=np.array([0.6924, 1.7310, 0.0433, 0.7270]),
+        densities=np.array([1858, 2243, 32, 1602]),
+        specific_heats=np.array(
+            [837.4] * 4
+        ),  # note the values in the paper are incorrect
+        resistances=np.array([0.0586, np.nan, np.nan, np.nan, np.nan, 0.1206]),
+        timestep=3600,
+    )
+
+    # fix the order of the ctfs for comparison with paper values
+    con._min_ctf_order = 5
+    con._max_ctf_order = 5
+
+    ctf_b = np.flip(np.array([0.002868, 0.053248, 0.060036, 0.007236, 0.000050, 0.0]))
+    ctf_d = np.flip(np.array([1.0, -1.175710, 0.300608, -0.015605, 0.000005, 0.0]))
+
+    # check the values of the bk and dk ctfs
+    # note: only these values are check as we use the improved ctf method
+    # so only the bk and dk values are expected to match
+    assert np.allclose(np.round(con.ctfs[1], 6), ctf_b, rtol=CTF_EPS, atol=0.0)
+    assert np.allclose(np.round(con.ctfs[3], 6), ctf_d, rtol=CTF_EPS, atol=0.0)
+
+    # check all the ctf sums are close to K
+    assert np.allclose(_calculate_sums(con.ctfs), con.thermal_transmittance)
+
+
+def test_heat_transfer_wang_wall_iv():
+    """Calculate properties of wall IV from:
+
+        Shengwei Wang, Youming Chen,
+        Transient heat flow calculation for multilayer constructions using
+            a frequency-domain regression method,
+        Building and Environment,
+        Volume 38, Issue 1,
+        2003,
+        Pages 45-61,
+        ISSN 0360-1323,
+        https://doi.org/10.1016/S0360-1323(02)00024-0.
+    """
+    con = Construction(
+        thicknesses=np.array([105, 100]) / 1000,
+        conductivities=np.array([0.840, 1.630]),
+        densities=np.array([1700, 2300]),
+        specific_heats=np.array([800, 1000]),
+        resistances=np.array([0.060, np.nan, 0.180, np.nan, 0.120]),
+        timestep=3600,
+    )
+
+    # fix the order of the ctfs for comparison with paper values
+    con._min_ctf_order = 5
+    con._max_ctf_order = 5
+
+    # fmt: off
+    qe_paper = np.array(
+        (
+            23.165, 21.194, 19.155, 17.146, 15.212, 13.375,
+            11.671, 10.166,  8.955,  8.112,  7.690,  7.709,
+             8.165,  9.107, 10.747, 13.210, 16.396, 19.982,
+            23.461, 26.138, 27.378, 27.325, 26.407, 24.950,
+        )
+    )
+    # fmt: on
+
+    ctf_b = np.flip(
+        np.array([0.000178, 0.013915, 0.043475, 0.018078, 0.001052, 0.000006])
+    )
+    ctf_d = np.flip(
+        np.array([1.0, -1.619841, 0.724516, -0.064305, 0.001542, -0.000006])
+    )
+
+    # check the values of the bk and dk ctfs
+    # note: only these values are check as we use the improved ctf method
+    # so only the bk and dk values are expected to match
+    assert np.allclose(np.round(con.ctfs[1], 6), ctf_b, rtol=CTF_EPS, atol=0.0)
+    assert np.allclose(np.round(con.ctfs[3], 6), ctf_d, rtol=CTF_EPS, atol=0.0)
+
+    # initialise values
+    qe = np.zeros(24)
+    qo = np.zeros(24)
+    temp_in = np.full_like(SOL_AIR, 273.15 + 24)
+    temp_out = SOL_AIR
+
+    # the paper values are after the 4th iteration
+    iterate_heat_transfer(con, temp_in, temp_out, qe, qo, iterations=4)
+
+    # check the heat transfer into the room
+    assert np.allclose(qe, qe_paper, rtol=0.0, atol=0.01)
+    assert np.allclose(qe, qe_paper, rtol=0.002, atol=0.0)
+
+    # now do further iterations to check heat transfer convergence
+    iterate_heat_transfer(con, temp_in, temp_out, qe, qo, iterations=96)
+
+    # check inside and outside heat transfer
+    assert np.allclose(
+        np.sum(qe) + np.sum(qo), 0.0, rtol=0.0, atol=HEAT_TRANSFER_SUM_EPS
+    )
+
+    # check all the ctf sums are close to K
+    assert np.allclose(_calculate_sums(con.ctfs), con.thermal_transmittance)
+
+    # check thermal transmittance value from paper
+    assert np.around(con.thermal_transmittance, 5) == 1.83033
+
+
+def test_construction_resistance_low():
+    """Calculate properties of a low capacitance wall."""
+    con = Construction(
+        thicknesses=np.array([1]) / 1000,
+        conductivities=np.array([30]),
+        densities=np.array([3950]),
+        specific_heats=np.array([192.5]),
+        resistances=np.array([0.060, np.nan, 0.020]),
+        timestep=3600,
+    )
+
+    # initialise values
+    qe = np.zeros(24)
+    qo = np.zeros(24)
+    temp_in = np.full_like(SOL_AIR, 273.15 + 24)
+    temp_out = SOL_AIR
+
+    # use a large number of iterations for convergence
+    iterate_heat_transfer(con, temp_in, temp_out, qe, qo, iterations=100)
+
+    # check inside and outside heat transfer
+    assert np.allclose(
+        np.sum(qe) + np.sum(qo), 0.0, rtol=0.0, atol=HEAT_TRANSFER_SUM_EPS
+    )
+
+    # check all the ctf sums are close to K
+    assert np.allclose(_calculate_sums(con.ctfs), con.thermal_transmittance)
+
+
+def test_heat_transfer_chen_wall_i():
+    """Wall I from Chen and Wang (2001) Appl. Math. Modelling 25."""
+    con = Construction(
+        thicknesses=np.array([240, 20]) / 1000,
+        conductivities=np.array([0.81, 0.70]),
+        densities=np.array([1800, 1600]),
+        specific_heats=np.array([880, 880]),
+        resistances=np.array([0.0546, np.nan, np.nan, 0.1149]),
+        timestep=3600,
+    )
+
+    # fmt: off
+    qe_paper = np.array(
+        (
+            26.2538, 24.2517, 22.1326, 20.0110, 17.9603, 16.0019,
+            14.1543, 12.4624, 11.0116,  9.8995,  9.1978,  8.9494,
+             9.1640,  9.8302, 11.0838, 13.1564, 16.1038, 19.7167,
+            23.5651, 27.0298, 29.2700, 29.9109, 29.3332, 28.0139,
+        )
+    )
+    # fmt: on
+
+    # initialise values
+    qe = np.zeros(24)
+    qo = np.zeros(24)
+    temp_in = np.full_like(SOL_AIR, 273.15 + 24)
+    temp_out = SOL_AIR
+
+    # the paper values are after the 4th iteration
+    iterate_heat_transfer(con, temp_in, temp_out, qe, qo, iterations=4)
+
+    # check the heat transfer into the room
+    assert np.allclose(qe, qe_paper, rtol=HEAT_TRANSFER_EPS, atol=0.0)
+
+    # now do further iterations to check heat transfer convergence
+    iterate_heat_transfer(con, temp_in, temp_out, qe, qo, iterations=96)
+
+    # check inside and outside heat transfer
+    assert np.allclose(
+        np.sum(qe) + np.sum(qo), 0.0, rtol=0.0, atol=HEAT_TRANSFER_SUM_EPS
+    )
+
+    # check all the ctf sums are close to K
+    assert np.allclose(_calculate_sums(con.ctfs), con.thermal_transmittance)
